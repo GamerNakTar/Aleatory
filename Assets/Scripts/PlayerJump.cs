eed28e0490191995241952665de0a0a77d9d3340
Minsang Kim <sksmsalstkd@naver.com>
2024-03-24 02:07:12 +0900

    Changes / Additions to Jump script
    -Coyote time, Jump Buffering 기능을 추가했습니다.
    -Ground detection 메커니즘을, Raycast에서 collision2d를 이용하는 방향으로 변경했습니다.
    -추락할시 무한정 가속이 붙지 않도록 terminal speed 변수를 추가했습니다.
    -점프 도중 상승, 하강시 속도를 조절할 수 있는 fall/jump Multiplier 변수를 추가했습니다.
    -점프키를 길게 누르면 높이 점프가 되는 기능을 구현해뒀습니다.

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Callbacks;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    [SerializeField] private float JumpPower;
+    // 이부분도 이식 필요
+    [SerializeField] private float jumpPower;
+    Vector2 vecGravity;
+    //
     private float CenterToBottom;
     Rigidbody2D rigid;
 
@@ -14,39 +19,48 @@ public class PlayerJump : MonoBehaviour
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        CenterToBottom = GetRaycastDistance();
+        
+        vecGravity = new Vector2(0, -Physics2D.gravity.y);
     }
 
-    public void Jump()
+    public void Jump(float jumpMult)
     {
-        rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+        //rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+        rigid.velocity = new Vector2(rigid.velocity.x, jumpPower * jumpMult);
     }    
 
     [SerializeField] private LayerMask groundLayer;
-    private bool isGrounded;
+    //[SerializeField] float jumpTime;
     [SerializeField] private float coyoteTime = 0.2f;
-    [SerializeField] private float coyoteTimeCounter;
     [SerializeField] private float jumpBufferTime = 0.2f;
-    [SerializeField] private float jumpBufferCounter;
     [SerializeField] private float terminalSpeed;
-    float GetRaycastDistance()
-    {
-        Collider2D playerCollider = GetComponent<Collider2D>();
-        if (playerCollider != null) {
-            float distanceToBottom = playerCollider.bounds.extents.y;
-            return distanceToBottom;
-            
-        }
-        return 1f;
-    }
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float fallMultiplier;
+    [SerializeField] private float jumpMultiplier;
+    
+    private bool isGrounded;
+    //private bool isJumping;
+    private float coyoteTimeCounter;
+    private float jumpBufferCounter;
+    //private float jumpCounter;
 
     void Update() { 
+        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.52f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
         
-        if (rigid.velocity.y < -15f) {
-            rigid.velocity = new Vector2(rigid.velocity.x, -15f);
+        
+
+        if (rigid.velocity.y < 0f) {
+            //isJumping = false;
+            rigid.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
+            if (rigid.velocity.y < -15f) {
+                rigid.velocity = new Vector2(rigid.velocity.x, -15f);
+            }
         }
+
+        //if (rigid.velocity.y > 0) {
+            //rigid.velocity += vecGravity * jumpMultiplier * Time.deltaTime;
+        //}
         
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, CenterToBottom, groundLayer);
         if(isGrounded) {
             coyoteTimeCounter = coyoteTime;
         } else  {
@@ -58,9 +72,16 @@ public class PlayerJump : MonoBehaviour
         } else {
             jumpBufferCounter -= Time.deltaTime;
         }
-        
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            if (rigid.velocity.y>0) {
+                rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * 0.6f);
+            }
+        }
+
         if(coyoteTimeCounter > 0f && jumpBufferCounter > 0f && isGrounded){ //랜덤하게 바뀌는 키로 어떻게 할당하는지 몰라 일단 할당해놨습니다
-            Jump();
+            Jump(jumpMultiplier);
+            //isJumping = true;
             jumpBufferCounter = 0f;
         }
     }    
