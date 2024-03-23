78b3dec648c6e4ec48820fd6343e9c13892cad58
Minsang Kim <sksmsalstkd@naver.com>
2024-03-24 02:07:45 +0900

    Changes / Additions to Jump script
    -Coyote time, Jump Buffering 기능을 추가했습니다.
    -Ground detection 메커니즘을, Raycast에서 collision2d를 이용하는 방향으로 변경했습니다.
    -추락할시 무한정 가속이 붙지 않도록 terminal speed 변수를 추가했습니다.
    -점프 도중 상승, 하강시 속도를 조절할 수 있는 fall/jump Multiplier 변수를 추가했습니다.
    -점프키를 길게 누르면 높이 점프가 되는 기능을 구현해뒀습니다.

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -12,20 +12,16 @@ public class PlayerJump : MonoBehaviour
     [SerializeField] private float jumpPower;
     Vector2 vecGravity;
     //
-    private float CenterToBottom;
     Rigidbody2D rigid;
 
-    
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        
         vecGravity = new Vector2(0, -Physics2D.gravity.y);
     }
 
     public void Jump(float jumpMult)
     {
-        //rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
         rigid.velocity = new Vector2(rigid.velocity.x, jumpPower * jumpMult);
     }    
 
@@ -47,8 +43,6 @@ public class PlayerJump : MonoBehaviour
     void Update() { 
         isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.52f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
         
-        
-
         if (rigid.velocity.y < 0f) {
             //isJumping = false;
             rigid.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
@@ -57,10 +51,6 @@ public class PlayerJump : MonoBehaviour
             }
         }
 
-        //if (rigid.velocity.y > 0) {
-            //rigid.velocity += vecGravity * jumpMultiplier * Time.deltaTime;
-        //}
-        
         if(isGrounded) {
             coyoteTimeCounter = coyoteTime;
         } else  {
@@ -79,9 +69,8 @@ public class PlayerJump : MonoBehaviour
             }
         }
 
-        if(coyoteTimeCounter > 0f && jumpBufferCounter > 0f && isGrounded){ //랜덤하게 바뀌는 키로 어떻게 할당하는지 몰라 일단 할당해놨습니다
+        if(coyoteTimeCounter > 0f && jumpBufferCounter > 0f && isGrounded){ 
             Jump(jumpMultiplier);
-            //isJumping = true;
             jumpBufferCounter = 0f;
         }
     }    
