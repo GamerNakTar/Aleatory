69eff958446f6cc510aff6a2967e2fba7357ed91
Whililik <mensa1229@gmail.com>
2024-04-04 18:27:26 +0900

    player horizontal movement control
    player horizontal movement control

--- Assets/Scripts/PlayerLeftRight.cs
+++ Assets/Scripts/PlayerLeftRight.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class PlayerLeftRight : MonoBehaviour
 {
 
-    public static void LeftRight(bool isLeft, Rigidbody2D rigid, float maxSpeed) {
+    public static void LeftRight(bool isLeft, Rigidbody2D rigid, float maxSpeed, float accelaration) {
         float h;
         if (isLeft)
         {
             h = -1;
         }
-        else {
+        else
+        {
             h = 1;
         }
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.right * h * accelaration, ForceMode2D.Force);
         if(rigid.velocity.x > maxSpeed) // right max speed
         {
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         }
-        else if (rigid.velocity.x < maxSpeed*(-1))// left max speed
+        else if (rigid.velocity.x < maxSpeed*(-1)) // left max speed
         {
             rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
         }
