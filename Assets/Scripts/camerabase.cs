5ced78cd1b3f79ddd46722be9c2da0302ee70af6
whililik <85292486+Seongho-Cho12@users.noreply.github.com>
2024-05-03 16:56:37 +0900

    Whililik (#11)
    * Player Left Right script
    
    Player Left Right script + minor change for Player Controller
    
    * Drunk guage UI and several changes in fever time(full guage of Drunk).
    
    Drunk guage UI 추가
    Full guage 일 때 화면 흔들림 추가
    캐릭터 좌우 이동 시 캐릭터 떨림 현상 제거
    
    ---------
    
    Co-authored-by: Whililik <mensa1229@gmail.com>
    Co-authored-by: 전현호 <64418708+GamerNakTar@users.noreply.github.com>

--- Assets/Scripts/camerabase.cs
+++ Assets/Scripts/camerabase.cs
@@ -7,6 +7,10 @@ public class CameraBase : MonoBehaviour
     // Public variables for customization
     public Transform targetPosition; // Reference to the target position the camera should follow
     public float speed = 5.0f;       // Speed of camera movement (adjust for desired smoothness)
+    [SerializeField] private GameObject player;
+    private NewBehaviourScript controllerScript;
+    private Vector3 rotateAngle;
+    private float rotateDirection = 1;
 
     private void Start()
     {
@@ -16,6 +20,7 @@ public class CameraBase : MonoBehaviour
             Debug.LogWarning("CameraBase: No target position assigned. Please set one in the inspector.");
             // Consider setting a default position here if necessary
         }
+        controllerScript = player.GetComponent<NewBehaviourScript>();
     }
 
     private void Update()
@@ -24,5 +29,43 @@ public class CameraBase : MonoBehaviour
         Vector3 vec3 = Vector3.Lerp(transform.position, targetPosition.position, speed * Time.deltaTime);
         vec3 = new Vector3(vec3.x, vec3.y, vec3.z-10);
         transform.position = vec3;
+
+        //If player is in Fever(hangover) time, camera will stumble
+        rotateAngle = transform.eulerAngles;
+        if(controllerScript.feverCurrTime > 0)
+        {
+            Debug.Log(rotateAngle);
+            if(rotateAngle.z < 30)
+            {
+                transform.Rotate(new Vector3(0, 0, rotateDirection), (30.0f - (rotateAngle.z * 1.3f)) * Time.deltaTime);
+            }
+            else if(rotateAngle.z > 330)
+            {
+                transform.Rotate(new Vector3(0, 0, rotateDirection), (30.0f - ((360 - rotateAngle.z) * 1.3f)) * Time.deltaTime);
+            }
+            if(rotateAngle.z > 20 && rotateAngle.z < 180)
+            {
+                rotateDirection = -1;
+            }
+            else if(rotateAngle.z < 340 && rotateAngle.z > 180)
+            {
+                rotateDirection = 1;
+            }
+        }
+        else
+        {
+            if(rotateAngle.z < 1 || rotateAngle.z > 359)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if(rotateAngle.z < 30)
+            {
+                transform.Rotate(new Vector3(0, 0, -1), (20.0f - 20/rotateAngle.z) * Time.deltaTime);
+            }
+            else if(rotateAngle.z > 330)
+            {
+                transform.Rotate(new Vector3(0, 0, 1), (20.0f - 20/(360 - rotateAngle.z)) * Time.deltaTime);
+            }
+        }
     }
 }
