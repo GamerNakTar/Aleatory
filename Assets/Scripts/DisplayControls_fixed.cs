e41fbf5cb71757cd253ac1eba34a78b11762e9e4
SATO2045 <jh123005@naver.com>
2024-05-10 14:03:02 +0900

    UI / FLAG

--- Assets/Scripts/DisplayControls_fixed.cs
+++ Assets/Scripts/DisplayControls_fixed.cs
@@ -14,7 +14,18 @@ public class DisplayControls_fixed : MonoBehaviour
 
     void Start()
     {
+<<<<<<< HEAD
+        // 화면 크기에 따라 Text UI의 크기를 조정
+        float screenRatioWidth = Screen.width / 1920f; // 1920은 기준 너비
+        float screenRatioHeight = Screen.height / 1080f; // 1080은 기준 높이
+        controlText.rectTransform.sizeDelta = new Vector2(100 * screenRatioWidth, 50 * screenRatioHeight);
 
+        Vector3 topRightCorner = new Vector3(Screen.width, Screen.height, 0);
+        Vector3 topRightWorld = Camera.main.ScreenToWorldPoint(topRightCorner);
+        transform.position = new Vector3(topRightWorld.x - 5 * screenRatioWidth , topRightWorld.y - 3 * screenRatioHeight, 0);
+=======
+
+>>>>>>> 913c1ca39d32579df28758c7bddde920f817ed67
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -59,10 +59,24 @@ public class NewBehaviourScript : MonoBehaviour
         currTime = 0f;
     }
 
+<<<<<<< HEAD
     void Start()
     {
         displayControls.UpdateKeyDisplay();
     }
+=======
+    public DisplayControls displayControls;
+
+    [SerializeField]
+    private float currTime;
+    public KeyCode Jump, MoveLeft, MoveRight;
+    public PlayerJump jumpHandler;
+
+    [SerializeField] public float timer;
+    [SerializeField] private float drunkTimer;
+    [SerializeField] private float drunkLength;
+    private bool isDrunk = false;
+>>>>>>> 913c1ca39d32579df28758c7bddde920f817ed67
 
     void Start()
     {
@@ -73,11 +87,14 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         currTime += Time.deltaTime;
+<<<<<<< HEAD
         if(isDrunk && !isDrunking)
         {
             StartCoroutine("DrunkCoroutine");
         }
 
+=======
+>>>>>>> 913c1ca39d32579df28758c7bddde920f817ed67
         if(isDrunk)
         {
             alcoholGauge -= Time.deltaTime * (alcoholCapacity / drunkLength);
@@ -119,6 +136,7 @@ public class NewBehaviourScript : MonoBehaviour
             MoveBehaviour.Move(false, rigid, maxSpeed, acceleration);
         }
     }
+
 
     private int getRandIdx(){
         System.Random rand = new System.Random();
